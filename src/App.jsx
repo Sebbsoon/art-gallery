4699ca877165d5fca8d9ae5a8bebb50b383e28aa
Sebastian <sebastian.thunberg@hotmail.se>
2025-09-01 14:41:38 +0200

    template setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header >
+        <h1 >My Gallery</h1>
+        <p>Images hosted on Google Drive</p>
+      </header>
+
+      <main >
+      </main>
+
+      <footer >
+        <p>Powered by GitHub Pages + Google Drive</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
